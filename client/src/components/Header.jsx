62bce205d03a9c998bb97224cdf21efd5f95571a
Maciej Wojtasiak <maciekwojtasiak@gmail.com>
2023-03-28 16:10:13 +0200

    first commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+        <div className="logo">Notes App</div>
+        <div className="nav">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
